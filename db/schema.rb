8f45d83faca3fa461213ef80ae69d802382e6ce6
David Collado <dmcollado@gmail.com>
2016-11-23 19:37:14 -0500

    abandon ship and remerge

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20161123192951) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
+    t.string   "email"
+    t.integer  "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
