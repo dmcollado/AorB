c1b1a3c203cb4897b59fc753b0df000d978c7852
David Collado <dmcollado@gmail.com>
2016-12-01 18:30:53 -0500

    fixed fb settings in omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   
   provider :twitter, ENV["TWITTER_API_KEY"], ENV["TWITTER_API_SECRET"]
 
-  provider :twitter, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"]
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
+    client_options: {
+      site: 'https://graph.facebook.com/v3.0',
+      authorize_url: "https://www.facebook.com/v3.0/dialog/oauth"
+    }
 
 end
